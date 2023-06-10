bb9e3f222c88b006ce8b590f948c55a27d2d537c
ZappyFountain <136033451+ZappyFountain@users.noreply.github.com>
2023-06-10 00:00:17 +0000

    modifed gradio

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import gradio as gr
 
 def calc_vo2(run_time, age, weight, height, gender):
-    if gender == "Male":
+    if gender[0] == "Male":
         gender_multiplier = 1
     else:
         gender_multiplier = 0
-    bmi = 703*(weight/(height**2))
-    time_sec = run_time/60
+    print(gender_multiplier)
+    bmi = 703*(int(weight)/int((height**2)))
+    time_sec = int(run_time)/60
     vo2 = (0.21 * age * gender_multiplier) - 0.84*(bmi) - 8.41*(time_sec) + 0.34*(time_sec**2) + 108.94
     return vo2
 
@@ -16,5 +17,14 @@ height1 = gr.Number(label = "Height in Inches")
 run_time1 = gr.Number(label = "Time spent running mile in seconds")
 gender1 = gr.CheckboxGroup(["Male", "Female"], label="Gender")
 
-demo= gr.Interface(calc_vo2, [run_time1, age1, weight1, height1, gender1], ["number"])
+with gr.Blocks() as demo:
+    gr.Markdown(
+    """
+    # VO2 Calculator
+    Please enter the data and press submit button
+    """)
+    inp = [run_time1, age1, weight1, height1, gender1]
+    out =["number"]
+    btn = gr.Button(value="Calculate")
+    btn.click(calc_vo2,inp,out)
 demo.launch()
